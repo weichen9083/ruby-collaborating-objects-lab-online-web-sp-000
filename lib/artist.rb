c1f33d95c4046c9ff1806f746c3b579817916afe
wei chen <weichen9083@gmail.com>
2020-12-08 02:45:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,41 +2,33 @@ class Artist
   
   attr_accessor :name 
   
-@@all = []
+  @@all = []
   
   def initialize(name)
-    @name = name 
-
-    save 
+    @name = name
+    self.class.all  << self 
+    
   end 
-  
-  def save 
-    @@all << self
-  end 
-  
   
   def self.all 
     @@all
   end 
   
-  def songs 
-  Song.all.select {|song| song.artist == self}
+  def add_song(song)
+    song.artist = self
   end 
   
-  def add_song(song)
-    song.artist = self 
+  def songs
+    Song.all.select {|song| song.artist == self}
   end 
-
+  
   def self.find_or_create_by_name(name)
-    
-    self.all.find {|artist| artist.name == name} || self.new(name)
-    
+    self.all.find {|artist_name|artist_name.name==name} || self.new(name)
+
   end 
   
   def print_songs
-    self.songs.each do |song| 
-      puts song.name
-    end 
+    self.songs.each {|song| puts song.name} 
   end 
-  
+    
 end 
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -22,3 +22,4 @@ class MP3Importer
   
   
 end 
+
